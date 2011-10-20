3d81aa893b5ecdb2c8ad74c96f004d856bd60ab9
Nico Hochberger <nico@irrwisch.net>
2011-10-20 23:05:47 +0200

    Added license information

--- src/hochberger/utilities/eventbus/SimpleEventBus.java
+++ src/hochberger/utilities/eventbus/SimpleEventBus.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Nico Hochberger.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Nico Hochberger - initial API and implementation
+ ******************************************************************************/
 package hochberger.utilities.eventbus;
 
 import hochberger.utilities.logging.LogToConsole;
