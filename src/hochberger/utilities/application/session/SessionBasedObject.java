79dedebf646d0753c68e74a9502a99ce343d0118
nhochberger <nico@irrwisch.net>
2014-07-07 20:22:03 +0200

    Introduced SessionBasedObject

---
+++ src/hochberger/utilities/application/session/SessionBasedObject.java
@@ -0,0 +1,21 @@
+package hochberger.utilities.application.session;
+
+import org.apache.log4j.Logger;
+
+public abstract class SessionBasedObject {
+
+	private final BasicSession session;
+
+	protected SessionBasedObject(BasicSession session) {
+		super();
+		this.session = session;
+	}
+
+	public BasicSession session() {
+		return session;
+	}
+
+	public Logger logger() {
+		return session().getLogger();
+	}
+}
