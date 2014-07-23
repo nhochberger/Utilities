f4ffae6f9960cba8538e851e7481be38ce38c1e5
nhochberger <nico@irrwisch.net>
2014-07-23 18:15:59 +0200

    added possibility to load properties form extern via LoadProperties

--- src/hochberger/utilities/properties/LoadProperties.java
+++ src/hochberger/utilities/properties/LoadProperties.java
@@ -4,13 +4,17 @@
  * are made available under the terms of the GNU Lesser Public License v2.1
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * 
+ *
  * Contributors:
  *     Nico Hochberger - initial API and implementation
  ******************************************************************************/
 package hochberger.utilities.properties;
 
+import hochberger.utilities.files.Closer;
+
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class LoadProperties {
@@ -23,5 +27,18 @@ public class LoadProperties {
         final Properties result = new Properties();
         result.load(ClassLoader.getSystemResourceAsStream(filePath));
         return result;
+    }
+
+    public static Properties fromExtern(final String filePath)
+            throws IOException {
+        final Properties result = new Properties();
+        InputStream inStream = null;
+        try {
+            inStream = new FileInputStream(filePath);
+            result.load(inStream);
+            return result;
+        } finally {
+            Closer.close(inStream);
+        }
     }
 }
