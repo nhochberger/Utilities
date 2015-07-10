5bfd064206fedf0c8dcb906bffbf562eae1d80a6
nhochberger <nico@irrwisch.net>
2015-07-10 17:43:47 +0200

    added support to timespans provided as float

--- src/hochberger/utilities/timing/ToMilis.java
+++ src/hochberger/utilities/timing/ToMilis.java
@@ -10,16 +10,31 @@ public class ToMilis {
         return 1000 * seconds;
     }
 
+    public static long seconds(final float seconds) {
+        return (long) (1000 * seconds);
+    }
+
     public static long minutes(final long minutes) {
         return minutes * seconds(60);
+    }
+
+    public static long minutes(final float minutes) {
+        return (long) (minutes * seconds(60));
     }
 
     public static long hours(final long hours) {
         return hours * minutes(60);
     }
 
+    public static long hours(final float hours) {
+        return (long) (hours * minutes(60));
+    }
+
     public static long days(final long days) {
         return days * hours(24);
     }
 
+    public static long days(final float days) {
+        return (long) (days * hours(24));
+    }
 }
