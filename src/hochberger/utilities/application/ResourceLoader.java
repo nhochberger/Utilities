5ba11115f935e79c8c72b89cc84c8aff23739cf2
nhochberger <nico@irrwisch.net>
2015-10-01 10:58:09 +0200

    workson Custos #2
    - deactivated method to load file from resource
    - added method to load stream from resource

--- src/hochberger/utilities/application/ResourceLoader.java
+++ src/hochberger/utilities/application/ResourceLoader.java
@@ -1,8 +1,10 @@
 package hochberger.utilities.application;
 
+import hochberger.utilities.exceptions.NotYetImplementedException;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.net.URISyntaxException;
+import java.io.InputStream;
 
 public class ResourceLoader {
 
@@ -10,12 +12,31 @@ public class ResourceLoader {
         super();
     }
 
+    public static InputStream loadAsStream(final String filePath) {
+        return ClassLoader.getSystemResourceAsStream(filePath);
+    }
+
     public static File loadFile(final String filePath)
             throws FileNotFoundException {
-        try {
-            return new File(ClassLoader.getSystemResource(filePath).toURI());
-        } catch (final URISyntaxException e) {
-            throw new FileNotFoundException(filePath);
-        }
+        throw new NotYetImplementedException();
+        // InputStream resourceAsStream = ClassLoader
+        // .getSystemResourceAsStream(filePath);
+        // String[] split = filePath.split("\\.");
+        // File tempFile = File.createTempFile("custos_tmp_", "."
+        // + split[split.length - 1]);
+        // FileWriter writer = new FileWriter(tempFile);
+        // InputStreamReader reader = new
+        // InputStreamReader(resourceAsStream);
+        // int readByte = -1;
+        // while (-1 != (readByte = reader.read())) {
+        // writer.write(readByte);
+        // }
+        // Closer.close(reader);
+        // Closer.close(writer);
+        // return tempFile;
+        // // return new
+        // File(ClassLoader.getSystemResource(filePath).toURI());
+        // // } catch (final URISyntaxException e) {
+        // // throw new FileNotFoundException(filePath);
     }
 }
