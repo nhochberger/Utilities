00901b5e2f48c0ada5445078bbb307aa98bcffae
nhochberger <nico@irrwisch.net>
2015-11-14 10:39:42 +0100

    workson Custos #42
    injected logger to GeoInformationProvider

--- src/hochberger/utilities/geo/GeoInformationProvider.java
+++ src/hochberger/utilities/geo/GeoInformationProvider.java
@@ -2,21 +2,29 @@ package hochberger.utilities.geo;
 
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
+
 import com.google.gson.Gson;
 
 public class GeoInformationProvider {
 
-    public GeoInformationProvider() {
+    private final Logger logger;
+
+    public GeoInformationProvider(final Logger logger) {
         super();
+        this.logger = logger;
     }
 
     public GeoInformation retrieveGeoInformation() {
         try {
             final GeoInformationRequest request = new GeoInformationRequest();
             final String result = request.performRequest();
-            return new Gson().fromJson(result, GeoInformation.class);
+            GeoInformation information = new Gson().fromJson(result,
+                    GeoInformation.class);
+            this.logger.info("Location retrieved: " + information);
+            return information;
         } catch (final IOException e) {
-            // geo information could not be obtained
+            this.logger.error("Unable to retrieve geo location information");
             return new NullGeoInformation();
         }
     }
