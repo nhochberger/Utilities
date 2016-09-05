f6d3ecd0aed1857b0128f7ab49a4641f911036c8
nhochberger <nico@irrwisch.net>
2016-09-05 12:27:40 +0200

    added loadStream

--- src/hochberger/utilities/application/ResourceLoader.java
+++ src/hochberger/utilities/application/ResourceLoader.java
@@ -1,10 +1,12 @@
 package hochberger.utilities.application;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.InputStreamReader;
+
+import hochberger.utilities.files.Closer;
 
 public class ResourceLoader {
 
@@ -16,33 +18,35 @@ public class ResourceLoader {
         return ClassLoader.getSystemResourceAsStream(filePath);
     }
 
-    public static File loadFile(final String filePath)
-            throws FileNotFoundException {
+    public static InputStream loadStream(final String filePath) {
+        return ClassLoader.getSystemResourceAsStream(filePath);
+    }
+
+    public static File loadFile(final String filePath) throws IOException {
         // throw new NotYetImplementedException();
-        final URL systemResource = ClassLoader.getSystemResource(filePath);
-        try {
-            final File file = new File(systemResource.toURI());
-            return file;
-        } catch (final URISyntaxException e) {
-            throw new FileNotFoundException(e.getMessage());
-        }
-        // InputStream resourceAsStream = ClassLoader
-        // .getSystemResourceAsStream(filePath);
-        // String[] split = filePath.split("\\.");
-        // File tempFile = File.createTempFile("custos_tmp_", "."
-        // + split[split.length - 1]);
-        // FileWriter writer = new FileWriter(tempFile);
-        // InputStreamReader reader = new
-        // InputStreamReader(resourceAsStream);
-        // int readByte = -1;
-        // while (-1 != (readByte = reader.read())) {
-        // writer.write(readByte);
+        // final URL systemResource = ClassLoader.getSystemResource(filePath);
+        // try {
+        // final File file = new File(systemResource.toURI());
+        // return file;
+        // } catch (final URISyntaxException e) {
+        // throw new FileNotFoundException(e.getMessage());
         // }
-        // Closer.close(reader);
-        // Closer.close(writer);
-        // return tempFile;
-        // // return new
-        // File(ClassLoader.getSystemResource(filePath).toURI());
+        final InputStream resourceAsStream = ClassLoader
+                .getSystemResourceAsStream(filePath);
+        final String[] split = filePath.split("\\.");
+        final File tempFile = File.createTempFile("custos_tmp_",
+                "." + split[split.length - 1]);
+        final FileWriter writer = new FileWriter(tempFile);
+        final InputStreamReader reader = new InputStreamReader(
+                resourceAsStream);
+        int readByte = -1;
+        while (-1 != (readByte = reader.read())) {
+            writer.write(readByte);
+        }
+        Closer.close(reader);
+        Closer.close(writer);
+        return tempFile;
+        // return new File(ClassLoader.getSystemResource(filePath).toURI());
         // // } catch (final URISyntaxException e) {
         // // throw new FileNotFoundException(filePath);
     }
