824152b1519bf5306d677b383dac64710db10495
nhochberger <nico@irrwisch.net>
2016-09-05 13:13:58 +0200

    - removed faulty implementation of loadFile - added loadStream instead

--- src/hochberger/utilities/application/ResourceLoader.java
+++ src/hochberger/utilities/application/ResourceLoader.java
@@ -1,12 +1,10 @@
 package hochberger.utilities.application;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 
-import hochberger.utilities.files.Closer;
+import hochberger.utilities.exceptions.NotYetImplementedException;
 
 public class ResourceLoader {
 
@@ -23,29 +21,22 @@ public class ResourceLoader {
     }
 
     public static File loadFile(final String filePath) throws IOException {
-        // throw new NotYetImplementedException();
-        // final URL systemResource = ClassLoader.getSystemResource(filePath);
-        // try {
-        // final File file = new File(systemResource.toURI());
-        // return file;
-        // } catch (final URISyntaxException e) {
-        // throw new FileNotFoundException(e.getMessage());
+        throw new NotYetImplementedException();
+        // final InputStream resourceAsStream = ClassLoader
+        // .getSystemResourceAsStream(filePath);
+        // final String[] split = filePath.split("\\.");
+        // final File tempFile = File.createTempFile("tmp_",
+        // "." + split[split.length - 1]);
+        // final FileWriter writer = new FileWriter(tempFile);
+        // final InputStreamReader reader = new InputStreamReader(
+        // resourceAsStream);
+        // int readByte = -1;
+        // while (-1 != (readByte = reader.read())) {
+        // writer.write(readByte);
         // }
-        final InputStream resourceAsStream = ClassLoader
-                .getSystemResourceAsStream(filePath);
-        final String[] split = filePath.split("\\.");
-        final File tempFile = File.createTempFile("custos_tmp_",
-                "." + split[split.length - 1]);
-        final FileWriter writer = new FileWriter(tempFile);
-        final InputStreamReader reader = new InputStreamReader(
-                resourceAsStream);
-        int readByte = -1;
-        while (-1 != (readByte = reader.read())) {
-            writer.write(readByte);
-        }
-        Closer.close(reader);
-        Closer.close(writer);
-        return tempFile;
+        // Closer.close(reader);
+        // Closer.close(writer);
+        // return tempFile;
         // return new File(ClassLoader.getSystemResource(filePath).toURI());
         // // } catch (final URISyntaxException e) {
         // // throw new FileNotFoundException(filePath);
